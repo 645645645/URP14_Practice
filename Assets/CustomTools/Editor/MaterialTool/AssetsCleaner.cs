b199f0bf43417382b516560b030dc1038823f9d9
645 <1009167021@qq.com>
2025-07-09 13:22:51 +0800

    变体清理兼容prefab内材质和内置shader

--- Assets/CustomTools/Editor/MaterialTool/AssetsCleaner.cs
+++ Assets/CustomTools/Editor/MaterialTool/AssetsCleaner.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
-using System.IO;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Text.RegularExpressions;
 using Unity.VisualScripting;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
@@ -27,23 +24,29 @@ namespace UnityEditor
     /// </summary>
     public class AssetsCleaner
     {
+
+        enum ToolsOrder
+        {
+            ClearMissScriptsInSelectionPrefab       = 1000,
+            ClearParticleUnuseMeshInSelectionPrefab = 1010,
+            ReserializeSelectionPrefab              = 1011,
+            ReserializeSelectionScene               = 1020,
+
+            ReserializeAllPrefab              = 2000,
+            ReserializeAllScene               = 2010,
+            ClearParticleUnuseMeshInAllPrefab = 2048,
+            CleanAllMaterials                 = 2050,
+            
+            Last                              = 3001,
+        }
         
         //ClearMaterialProperties 不删除_MainTex(而是置空)，UI.maintexture需要
         private static readonly string[] ExcudeMaterialTexProperties = { "_MainTex" };
-        
-        // 没有CGPROGRAM/ENDCG部分的Pass代码不会有keyword定义，因此可忽略
-        private static string codePattern = @"Pass[\r\n\s\t]*\{[\r\n\s\t]*(?<passInfo>\S((?!ENDCG)[\s\S])*)CGPROGRAM(\r\n|\r|\n)(?<passCode>((?!ENDCG)[\s\S])+)ENDCG[\r\n\s\t]*\}";
-        
-        // 提取multi_compile keyword
-        private static string multiCompilePattern = @"#pragma multi_compile\s+([_]+\s+)?(?<multiCompile>[a-zA-Z0-9_]+(\s+[a-zA-Z0-9_]+)*)";
-        
-        // 提取shader_feature keyword
-        private static string shaderFeaturePattern = @"#pragma shader_feature\s+([_]+\s+)?(?<shaderFeature>[a-zA-Z0-9_]+(\s+[a-zA-Z0-9_]+)*)";
 
-        // Shader Keywords缓存(Key: shaderPath; Value: shader Keywords)
-        private static Dictionary<string, HashSet<string>> shaderKeywordsCache;//to File
+        // Shader Keywords缓存(Key: shaderName; Value: shader Keywords)
+        private static Dictionary<string, string[]> shaderKeywordsCache;
 
-        [MenuItem("GameObject/清理当前场景中的 MissScripts", false, 2000)]
+        [MenuItem("GameObject/当前场景 清理 MissScripts", false, (int)ToolsOrder.Last)]
         public static void ClearMissScriptsInCurrentScene()
         {
             Scene        currentScene = SceneManager.GetActiveScene();
@@ -76,7 +79,7 @@ namespace UnityEditor
 
 
         
-        [MenuItem("Assets/Assets Cleaner/清理选中Prefab里的 MissScripts", false, 1900)]
+        [MenuItem("Assets/Assets Cleaner/选中Prefab 清理 MissScripts", false, (int)ToolsOrder.ClearMissScriptsInSelectionPrefab)]
         public static void ClearMissScriptsInSelectionPrefab()
         {
             var gos = Selection.GetFiltered<GameObject>(SelectionMode.DeepAssets)
@@ -108,7 +111,7 @@ namespace UnityEditor
         }
         
         
-        [MenuItem("Assets/Assets Cleaner/重新序列化选中Prefab", false, 1901)]
+        [MenuItem("Assets/Assets Cleaner/选中Prefab 重新序列化", false, (int)ToolsOrder.ReserializeSelectionPrefab)]
         public static void ReserializeSelectionPrefab()
         {
             var gos = Selection.GetFiltered<Object>(SelectionMode.DeepAssets)
@@ -133,7 +136,7 @@ namespace UnityEditor
         }
         
 
-        [MenuItem("Assets/Assets Cleaner/清理选中Prefab里没用到的Mesh", false, 1910)]
+        [MenuItem("Assets/Assets Cleaner/选中Prefab 清理没用到的Mesh", false, (int)ToolsOrder.ClearParticleUnuseMeshInSelectionPrefab)]
         public static void ClearParticleUnuseMeshInSelectionPrefab()
         {
             var gos = Selection.GetFiltered<GameObject>(SelectionMode.DeepAssets)
@@ -170,7 +173,7 @@ namespace UnityEditor
         }
         
 
-        [MenuItem("Assets/Assets Cleaner/重新序列化选中Scene", false, 1920)]
+        [MenuItem("Assets/Assets Cleaner/选中Scene 重新序列化", false, (int)ToolsOrder.ReserializeSelectionScene)]
         public static void ReserializeSelectionScene()
         {
             var gos = Selection.GetFiltered<SceneAsset>(SelectionMode.DeepAssets);
@@ -191,39 +194,11 @@ namespace UnityEditor
         }
         
         
-        [MenuItem("Assets/Assets Cleaner/重新序列化所有Prefab", false, 2000)]
-        public static void ReserializeAllPrefab()
-        {
-            ReserializeAllAssets("prefab");
-        }
-        
-
-        
-        [MenuItem("Assets/Assets Cleaner/重新序列化所有Scene", false, 2010)]
-        public static void ReserializeAllScene()
-        {
-            ReserializeAllAssets("scene");
-        }
         
         
-        private static void ReserializeAllAssets(string type)
-        {
-            AssetDatabase.StartAssetEditing();
-            var paths = AssetDatabase.FindAssets($"t:{type}", new string[] { "Assets" })
-                                      .Select(AssetDatabase.GUIDToAssetPath);
+        
 
-            //能去掉过时的字段，同时也会补上可空字段（带默认值），SetDirty也一样 罢了 就这样吧
-            AssetDatabase.ForceReserializeAssets(paths, ForceReserializeAssetsOptions.ReserializeAssets);
-            
-            AssetDatabase.StopAssetEditing();
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-            
-            Debug.LogFormat("<color=green> Reserialize All {0} Assets Success. </color>", type.FirstCharacterToUpper());
-        }
-
-
-        [MenuItem("Assets/Assets Cleaner/清理所有Prefab里没用到的Mesh", false, 2001)]
+        [MenuItem("Assets/Assets Cleaner/所有Prefab 清理没用到的Mesh", false, (int)ToolsOrder.ClearParticleUnuseMeshInAllPrefab)]
         public static void ClearParticleUnuseMeshInAllPrefab()
         {
             var paths = AssetDatabase.FindAssets("t:prefab", new string[] { "Assets" })
@@ -260,8 +235,40 @@ namespace UnityEditor
         }
         
         
+        [MenuItem("Assets/Assets Cleaner/所有Prefab 重新序列化", false, (int)ToolsOrder.ReserializeAllPrefab)]
+        public static void ReserializeAllPrefab()
+        {
+            ReserializeAllAssets("prefab");
+        }
         
-        [MenuItem("Assets/Assets Cleaner/清理所有Material无用关键词与属性", false, 2020)]
+
+        
+        [MenuItem("Assets/Assets Cleaner/所有Scene 重新序列化", false, (int)ToolsOrder.ReserializeAllScene)]
+        public static void ReserializeAllScene()
+        {
+            ReserializeAllAssets("scene");
+        }
+        
+        
+        private static void ReserializeAllAssets(string type)
+        {
+            AssetDatabase.StartAssetEditing();
+            var paths = AssetDatabase.FindAssets($"t:{type}", new string[] { "Assets" })
+                                      .Select(AssetDatabase.GUIDToAssetPath);
+
+            //能去掉过时的字段，同时也会补上可空字段（带默认值），SetDirty也一样 罢了 就这样吧
+            AssetDatabase.ForceReserializeAssets(paths, ForceReserializeAssetsOptions.ReserializeAssets);
+            
+            AssetDatabase.StopAssetEditing();
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            
+            Debug.LogFormat("<color=green> Reserialize All {0} Assets Success. </color>", type.FirstCharacterToUpper());
+        }
+        
+        
+        
+        [MenuItem("Assets/Assets Cleaner/所有Material 清理无用关键词与属性", false, (int)ToolsOrder.CleanAllMaterials)]
         public static void CleanAllMaterials()
         {
             System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
@@ -269,7 +276,7 @@ namespace UnityEditor
             
             if (shaderKeywordsCache == null)
             {
-                shaderKeywordsCache = new Dictionary<string, HashSet<string>>();
+                shaderKeywordsCache = new Dictionary<string, string[]>();
             }
             else
             {
@@ -282,6 +289,8 @@ namespace UnityEditor
 
             (int clearMaterialCount, int clearPropertyCount, int clearKeyWorldCount) = (0, 0, 0);
 
+            Object     obj;
+            Material[] materials;
             Material material;
 
             string path;
@@ -289,14 +298,45 @@ namespace UnityEditor
             for (int i = 0; i < materialPaths.Count; i++)
             {
                 path = materialPaths[i];
-                
-                material = AssetDatabase.LoadAssetAtPath<Material>(path);
 
-                if (CleanMaterialSerializedProperty(material, ref clearPropertyCount, ref clearKeyWorldCount))
+                //某些prefab里存多个material
+                if (path.EndsWith(".prefab") || path.EndsWith(".fbx"))
                 {
-                    clearMaterialCount++;
+                    obj = AssetDatabase.LoadAssetAtPath(path, typeof(Object));
+                    materials = AssetDatabase.LoadAllAssetsAtPath(path)
+                                             .Select(x=> x as Material)
+                                             .ToArray();
 
-                    Debug.LogFormat("CleanMaterial: {0}", path);
+                    bool isDirty = false;
+                    for (int j = 0; j < materials.Length; j++)
+                    {
+                        material = materials[j];
+                        if (material && CleanMaterialSerializedProperty(material, ref clearPropertyCount, ref clearKeyWorldCount))
+                        {
+                            clearMaterialCount++;
+
+                            EditorUtility.SetDirty(material);
+
+                            isDirty = true;
+                            
+                            Debug.LogFormat("CleanMaterial: path = {0}, material = {1}", path, material.name);
+                        }
+                    }
+
+                    if (isDirty) EditorUtility.SetDirty(obj);
+                }
+                else
+                {
+                    material = AssetDatabase.LoadAssetAtPath<Material>(path);
+
+                    if (CleanMaterialSerializedProperty(material, ref clearPropertyCount, ref clearKeyWorldCount))
+                    {
+                        clearMaterialCount++;
+
+                        EditorUtility.SetDirty(material);
+
+                        Debug.LogFormat("CleanMaterial: {0}", path);
+                    }
                 }
             }
 
@@ -315,6 +355,7 @@ namespace UnityEditor
         
         private static bool CleanMaterialSerializedProperty(Material material, ref int propertyCount, ref int keyWorldCount)
         {
+            
             using SerializedObject so = new SerializedObject(material);
 
             SerializedProperty saveProperty = so.FindProperty("m_SavedProperties");
@@ -336,18 +377,20 @@ namespace UnityEditor
         private static bool RemoveKeyWord(Material material, ref int keyWorldCount)
         {
             bool result = false;
-            if (material.shader)
-            {
-                string shaderPath = AssetDatabase.GetAssetPath(material.shader);
 
-                bool isRecord = shaderKeywordsCache.TryGetValue(shaderPath, out var shaderKeywords);
-                
+            Shader shader = material.shader;
+            
+            if (shader)
+            {
+                //能处理Resources/unity_builtin_extra
+                bool isRecord = shaderKeywordsCache.TryGetValue(shader.name, out var shaderKeywords);
+
                 if (!isRecord)
                 {
-                    shaderKeywords = GetShaderKeywords(shaderPath);
-                    shaderKeywordsCache.Add(shaderPath, shaderKeywords);
+                    shaderKeywords = GetShaderKeywords(shader);
+                    shaderKeywordsCache.Add(shader.name, shaderKeywords);
                 }
-                
+
                 if (shaderKeywords != null)
                 {
                     List<string> matKeywords = new List<string>(material.shaderKeywords);
@@ -360,68 +403,24 @@ namespace UnityEditor
                         keyWorldCount++;
                         result = true;
                     }
-                    if(result)
+
+                    if (result)
                     {
                         material.shaderKeywords = matKeywords.ToArray();
-                        EditorUtility.SetDirty(material);
-                        AssetDatabase.SaveAssetIfDirty(material);
                     }
                 }
             }
 
             return result;
         }
+        
+        private static readonly System.Reflection.MethodInfo method =
+            typeof(ShaderUtil).GetMethod("GetShaderGlobalKeywords",
+                                         System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
 
-        private static HashSet<string> GetShaderKeywords(string shaderPath)
+        private static string[] GetShaderKeywords(Shader shader)
         {
-            HashSet<string> totalKeywords = new HashSet<string>();
-
-            try
-            {
-                StreamReader sr         = new StreamReader(shaderPath, Encoding.Default);
-                string       shaderCode = sr.ReadToEnd();
-
-                MatchCollection codeMatches;
-                MatchCollection multiCompileMatches;
-                MatchCollection shaderFeatureMatches;
-                codeMatches = Regex.Matches(shaderCode, codePattern);
-
-                string passCode;
-                string multiCompileKeywords;
-                string shaderFeatureKeywords;
-
-                // 分析每一个Pass
-                for (int i = 0; i < codeMatches.Count; ++i)
-                {
-                    passCode = codeMatches[i].Groups["passCode"].Value;
-
-                    multiCompileMatches = Regex.Matches(passCode, multiCompilePattern);
-                    for (int j = 0; j < multiCompileMatches.Count; ++j)
-                    {
-                        multiCompileKeywords = multiCompileMatches[j].Groups["multiCompile"].Value;
-                        if (!string.IsNullOrEmpty(multiCompileKeywords))
-                        {
-                            totalKeywords.AddRange(multiCompileKeywords.Split(' '));
-                        }
-                    }
-
-                    shaderFeatureMatches = Regex.Matches(passCode, shaderFeaturePattern);
-                    for (int k = 0; k < shaderFeatureMatches.Count; ++k)
-                    {
-                        shaderFeatureKeywords = shaderFeatureMatches[k].Groups["shaderFeature"].Value;
-                        if (!string.IsNullOrEmpty(shaderFeatureKeywords))
-                        {
-                            totalKeywords.AddRange(shaderFeatureKeywords.Split(' '));
-                        }
-                    }
-                }
-
-            }
-            catch
-            {
-            }
-
-            return totalKeywords;
+            return method?.Invoke(null, new object[] { shader }) as string[];
         }
 
         
